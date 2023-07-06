d50f645631dbec43fc70359f5cebf8fcdbe5348f
Thomas Coopman <thomas.coopman@gmail.com>
2023-07-06 10:29:54 +0200

    basic setup of Scenario with failing test

--- Sharingo.Tests/SharingoTests.cs
+++ Sharingo.Tests/SharingoTests.cs
@@ -8,8 +8,50 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void SanityTest()
     {
         Assert.Pass();
     }
+
+    [Test]
+    public void Given_no_bikes_When_reserving_a_bike_Then_reservation_should_fail()
+    {
+        new Scenario()
+        .Given(new List<IEvent>())
+        .When(new ReserveAnyBikeAtStation())
+        .Then(new List<IEvent>() { new NoBikeReserved() });
+    }
+}
+
+public class Scenario
+{
+    public Scenario When(ReserveAnyBikeAtStation reserveAnyBikeAtStation)
+    {
+        return this;
+    }
+
+    public Scenario Given(List<IEvent> events)
+    {
+        return this;
+    }
+
+    public void Then(List<IEvent> events)
+    {
+        Assert.Fail();
+    }
+}
+
+public interface IEvent
+{
+
+}
+
+public record NoBikeReserved : IEvent
+{
+
+}
+
+public record ReserveAnyBikeAtStation
+{
+
 }
